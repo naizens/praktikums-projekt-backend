d176fb91c2ace26f07db1c4adcff76344dbbefe3
Niclas Heide <niclas.heide@students.tbs1.de>
2022-07-12 11:55:15 +0200

    added login page

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -3,10 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Models\Person;
+use App\Models\PersonHoliday;
 
 class PersonController extends Controller
 {
     public function index(){
         return response()->json(Person::with(["holidays"])->get());
+    }
+    public function submit(\Illuminate\Http\Request $request){
+        $inputs = $request->all();
+        $holiday = new PersonHoliday([
+            "start"=>$inputs["startDate"],
+            "end"=>$inputs["endDate"],
+            "type"=>$inputs["holidaytype"],
+            "daytime"=>$inputs["daytime"]
+        ]);
+        $holiday->save();
+        return redirect("/kalender");
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -72,9 +72,9 @@ $app->configure('app');
 |
 */
 
-// $app->middleware([
-//     App\Http\Middleware\ExampleMiddleware::class
-// ]);
+$app->middleware([
+    \Illuminate\Session\Middleware\StartSession::class
+]);
 
 // $app->routeMiddleware([
 //     'auth' => App\Http\Middleware\Authenticate::class,
