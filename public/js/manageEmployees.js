c5d03d7a59b7dabb9c5751d4b4ba99950c4a0638
Niclas Heide <niclas.heide@students.tbs1.de>
2022-08-03 11:34:18 +0200

    added modal to add Users

---
+++ public/js/manageEmployees.js
@@ -0,0 +1,78 @@
+// Language: javascript
+"use strict";
+
+// Modal Part ---------------------------------------------------------------
+let openModal = document.querySelectorAll(".addUser");
+openModal.forEach((entry) =>{
+    entry.addEventListener("click", function (event) {
+        event.preventDefault();
+        toggleModal();
+    });
+});
+// Select the modal-overlay and add the eventlistener to it
+const overlay = document.querySelector('.modal-overlay');
+overlay.addEventListener('click', toggleModal);
+// Select the modal-close buttons and add a eventlistener to them
+const closeModal = document.querySelectorAll('.modal-close')
+for (let i = 0; i < closeModal.length; i++) {
+    closeModal[i].addEventListener('click', toggleModal);
+}
+// Toggle the modal when the user clicks on the button or anywhere outside of it
+document.onkeydown = function(evt) {
+    evt = evt || window.event
+    let isEscape = false
+    if ("key" in evt) {
+        isEscape = (evt.key === "Escape" || evt.key === "Esc");
+    } else {
+        isEscape = (evt.keyCode === 27);
+    }
+    if (isEscape && document.body.classList.contains('modal-active')) {
+        toggleModal();
+    }
+    if (isEscape && document.body.classList.contains('overflow-hidden')) {
+        toggleModal();
+    }
+};
+// Function for the toggling of the modal
+function toggleModal (clickedDate, clickEvent) {
+    const body = document.querySelector('body');
+    const modal = document.querySelector('.modal');
+    modal.classList.toggle('opacity-0');
+    modal.classList.toggle('pointer-events-none');
+    body.classList.toggle('modal-active');
+    body.classList.toggle('overflow-hidden');
+    if(clickEvent){
+        document.querySelector("[name='startDate']").value = clickedDate;
+    }
+}
+//Show Radio Buttons for which time of the halfDay
+const halfDayRadio = document.querySelectorAll("#halfDayRadio, #fullDayRadio");
+halfDayRadio.forEach((input) =>{
+    input.addEventListener("click", function(event) {
+        if(input.checked && input.value === "halfDay") {
+            document.querySelectorAll(".halfDayRadios").forEach((entry) =>{
+                entry.classList.add("flex");
+                entry.classList.remove("hidden");
+            });
+        }
+        else {
+            document.querySelectorAll(".halfDayRadios").forEach((entry) =>{
+                entry.classList.remove("flex");
+                entry.classList.add("hidden");
+            })
+        }
+    })
+});
+// End of Modal Part ---------------------------------------------------------
+// Select the add button and add the eventlistener to it
+const addButton = document.querySelector("#addButton");
+addButton.addEventListener("click", addUser);
+
+function addUser() {
+    let form = document.getElementById("userForm");
+    let formData = new FormData(form)
+
+    console.log(form);
+
+    
+}
--- resources/views/templates/manageEmployees.blade.php
+++ resources/views/templates/manageEmployees.blade.php
@@ -16,7 +16,7 @@
             </div>
             <div class="mx-6 h-6 w-px bg-gray-300"></div>
             <div class="hidden md:flex md:items-center">
-                <button type="button" class="addEvent focus:outline-none rounded-md border border-transparent bg-netzfactor py-2 px-4 text-sm font-medium text-white shadow-md hover:bg-netzfactor-light focus:ring-2 focus:ring-netzfactor focus:ring-offset-2">Nutzer Anlegen</button>
+                <button type="button" class="addUser focus:outline-none rounded-md border border-transparent bg-netzfactor py-2 px-4 text-sm font-medium text-white shadow-md hover:bg-netzfactor-light focus:ring-2 focus:ring-netzfactor focus:ring-offset-2">Nutzer Anlegen</button>
             </div>
 
             <div class="relative md:hidden buttoncontainer">
@@ -44,13 +44,76 @@
                         <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
                         <a href="#" class="addEvent text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-0-item-0">Nutzer Anlegen</a>
                     </div>
-                    <div class="py-1" role="none">
-                        <a href="#" class="thisMonth text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-0-item-1">Gehe zu Heute</a>
-                    </div>
                 </div>
             </div>
         </div>
     </header>
     <main>
+        <!-- Modal -->
+        <div class="modal opacity-0 pointer-events-none fixed w-full h-full top-0 left-0 flex items-center justify-center z-20">
+            <div class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
+            <div class="modal-container w-11/12 md:max-w-md sm:max-h-[80vh] mx-auto shadow-lg z-50 overflow-y-auto scrollbar">
+                <div class="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50">
+                    <svg class="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+                        <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+                    </svg>
+                    <span class="text-sm">(Esc)</span>
+                </div>
+                <div class="modal-content bg-white py-4 text-left px-6 border-2 border-slate-300 shadow-sm rounded-md" >
+                    <!--Title-->
+                    <div class="flex justify-between items-center pb-4 border-b border-slate-300" >
+                        <div class="flex justify-between items-center">
+                            <img src="/img/event.png" class="w-10 h-10 mr-2" alt="event">
+                            <p class="text-xl text-center font-semibold">Nutzer Anlegen</p>
+                        </div>
+
+                        <div class="modal-close cursor-pointer z-50 p-1 border border-slate-300 rounded-md hover:bg-gray-100 shadow-sm">
+                            <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+                                <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+                            </svg>
+                        </div>
+                    </div>
+
+            <!--Body-->
+                    <form action="/manageEmployees/submit" method="post" id="userForm" class="py-3">
+                        <label class="my-3 p-2 border border-slate-300 rounded-md shadow-sm flex flex-col justify-center text-lg font-medium">
+                            <span>Name:</span>
+                            <input required type="text" name="firstName" class="firstName border border-slate-200 rounded-md p-1">
+                        </label>
+                        <label class="my-3 p-2 border border-slate-300 rounded-md shadow-sm flex flex-col justify-center text-lg font-medium">
+                            <span>Nachname:</span>
+                            <input required type="text" name="lastName" class="lastName shadow-sm border border-slate-200 rounded-md p-1">
+                        </label>
+                        <label class="my-3 p-2 border border-slate-300 rounded-md shadow-sm flex flex-col justify-center text-lg font-medium">
+                            <span>Geburtstag:</span>
+                            <input required type="date" name="birthdate" class="shadow-sm border border-slate-200 rounded-md p-1">
+                        </label>
+                        <label class="my-3 p-2 border border-slate-300 rounded-md shadow-sm flex flex-col justify-center text-lg font-medium">
+                            <span>E-Mail:</span>
+                            <input required type="email" name="eMail" class="lastName shadow-sm border border-slate-200 rounded-md p-1">
+                        </label>
+                        <label class="my-3 p-2 border border-slate-300 rounded-md shadow-sm flex flex-col justify-center text-lg font-medium">
+                            <span>Abteilung:</span>
+                            <select name="department" required class="shadow-sm border border-slate-200 rounded-md p-1">
+                                <option value="1">Web</option>
+                                <option value="2">App</option>
+                                <option value="3">Network</option>
+                                <option value="4">Media</option>
+                            </select>
+                        </label>
+                        <label class="my-3 p-2 border border-slate-300 rounded-md shadow-sm flex flex-col justify-center text-lg font-medium">
+                            <span>Urlaubstage:</span>
+                            <input required type="number" name="maxAmountOfHolidays" class="shadow-sm border border-slate-200 rounded-md p-1">
+                        </label>
+                    </form>
+            <!--Footer-->
+                    <div class="flex justify-end border-t border-slate-300 pt-4">
+                        <button id="addButton" class="border border-slate-300 rounded-lg bg-netzfactor py-2 px-4 shadow-md text-white hover:bg-netzfactor-light mr-2">Hinzufügen</button>
+                        <button class="modal-close border border-slate-300 rounded-lg bg-transparent py-2 px-4 shadow-md  text-netzfactor hover:bg-gray-100 hover:text-netzfactor-light">Schließen</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
+    <script type="text/javascript" async src="/js/manageEmployees.js"></script>
 @endsection
