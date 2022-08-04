f810c494c47678d36a87658bfea4eb20f7bcacf3
Niclas Heide <niclas.heide@students.tbs1.de>
2022-08-04 16:24:04 +0200

    font color based on department

--- public/js/base.js
+++ public/js/base.js
@@ -2,7 +2,8 @@
 const department = document.querySelector("#departmentBg");
 const profilePicture = document.querySelector("#profilePicture");
 let bg,
-    ring;
+    ring,
+    font;
 if(currentUser.department === "web"){
     bg = "bg-web"
     ring = "ring-web"
@@ -14,14 +15,22 @@ if(currentUser.department === "web"){
 } else if (currentUser.department === "network"){
     bg = "bg-network"
     ring = "ring-network"
+    font = "text-slate-100"
     department.innerHTML = "[NF]-Net"
 } else if (currentUser.department === "media"){
     bg = "bg-media"
     ring = "ring-media"
+    font = "text-slate-100"
     department.innerHTML = "[NF]-Media"
+} else{
+    bg = "bg-netzfactor"
+    ring = "ring-netzfactor"
+    font = "text-slate-100"
+    department.innerHTML = "[NF]"
 }
 department.classList.add(bg);
 profilePicture.classList.add(ring);
+department.classList.add(font);
 
 
 const profileButton = document.querySelector("#profileButton");
